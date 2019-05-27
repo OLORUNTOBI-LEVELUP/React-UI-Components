3c99d98053673ce938ef142b7be839249bda8807
oloruntobi <awoderuoloruntobi@gmail.com>
2019-05-27 16:25:42 +0100

    Completed Task 1 and 2 I completed the header and card sections, I also used moment in my header. I also added the footer icons

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,17 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+const HeaderContent = () => {
+  return (
+    <div className="titlecontent">
+      <p>
+        Lets learn React by building simple Interfaces with components. Don't
+        try to overthink it, Just keep it simple and fun. Once you feel
+        comfortable using components, You are well on your way to mastering
+        react.
+      </p>
+    </div>
+  );
+};
+
+export default HeaderContent;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,17 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+import moment from 'moment';
+
+const HeaderTitle = () => {
+  return (
+ 
+      <div className="content">
+        <h5>Lambda School </h5>
+        <h6>@lambdaschool . {moment().format("MMM Do YY")}</h6>
+      </div>
+      
+    
+  );
+};
+
+export default HeaderTitle;
